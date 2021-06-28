b102041311ad7461e8d3dff7ccdc177e28a3bab3
Sushant <sushantverma9648@gmail.com>
2021-06-28 15:49:23 +0530

    images in code

--- rps_main.py
+++ rps_main.py
@@ -8,8 +8,17 @@ rpsgame.title("Rock Paper Scissor")
 rpsgame.configure(background="#42CFEF")
 
 # images
-rock_img_user = ImageTk.PhotoImage(Image.open("rock-user.png"))
+rock_user = ImageTk.PhotoImage(Image.open("data\\rock-user.png"))
+paper_user = ImageTk.PhotoImage(Image.open("data\\paper-user.png"))
+scissor_user = ImageTk.PhotoImage(Image.open("data\\scissors-user.png"))
+rock_comp = ImageTk.PhotoImage(Image.open("data\\rock.png"))
+paper_comp = ImageTk.PhotoImage(Image.open("data\\paper.png"))
+scissor_comp = ImageTk.PhotoImage(Image.open("data\\scissors.png"))
 
-
+# insertion of images
+user_label = Label(rpsgame, image=scissor_user, bg="#42CFEF")
+comp_label = Label(rpsgame, image=scissor_comp, bg="#42CFEF")
+comp_label.grid(row=1, column=0)
+user_label.grid(row=1, column=4)
 
 rpsgame.mainloop()
