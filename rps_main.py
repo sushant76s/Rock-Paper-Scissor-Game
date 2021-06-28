d3fd3cd6be5d36f9ae0ecc62a02872c680fa014b
Sushant <sushantverma9648@gmail.com>
2021-06-28 14:59:54 +0530

    game window

--- rps_main.py
+++ rps_main.py
@@ -1,10 +1,15 @@
 from tkinter import*
+from PIL import Image, ImageTk
 
 
-# main Window
-root = Tk()
+# main game window
+rpsgame = Tk()
+rpsgame.title("Rock Paper Scissor")
+rpsgame.configure(background="#42CFEF")
+
+# images
+rock_img_user = ImageTk.PhotoImage(Image.open("rock-user.png"))
 
 
 
-
-root.mainloop()
+rpsgame.mainloop()
